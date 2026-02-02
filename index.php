2d9ce9936cb9f54da76e63b351671f9d00ba214d
Noah Gerard <97645851+noahgerard@users.noreply.github.com>
2026-02-02 15:34:54 -0500

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-
-# Load environment variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-readfile("./frontEndFiles/HTML/index.html");
