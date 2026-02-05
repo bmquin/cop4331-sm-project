78b70dd0b2b41c8569e5ceeddda8a33b44b1dcaf
DANNYSTARLORD <161342908+DANNYSTARLORD@users.noreply.github.com>
2026-02-05 15:30:28 -0500

    Fixed the autocomplete killing UI

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,23 @@
-const authModal = document.querySelector('.auth-modal');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
+const authModal = document.querySelector(".auth-modal");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
 
-const alertBox = document.querySelector('.alert-box');
+const alertBox = document.querySelector(".alert-box");
 
-registerLink.addEventListener('click', () => {
-  authModal.classList.add('active');
+registerLink.addEventListener("click", () => {
+  authModal.classList.add("active");
 });
 
-loginLink.addEventListener('click', () => {
-  authModal.classList.remove('active');
+loginLink.addEventListener("click", () => {
+  authModal.classList.remove("active");
 });
 
-setTimeout(() => alertBox.classList.add('show'), 50);
+if (loginBtnModal)
+  loginBtnModal: addEventListener("click", () => authModal.classList("show"));
+
+setTimeout(() => alertBox.classList.add("show"), 50);
 
 setTimeout(() => {
-  alertBox.classList.remove('show');
-  setTimeout(()=> alertBox.remove(), 500)
+  alertBox.classList.remove("show");
+  setTimeout(() => alertBox.remove(), 500);
 }, 3000);
