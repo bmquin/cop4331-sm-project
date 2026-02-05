9a894a24b4a3a91b928987083c283962cfdd1356
DANNYSTARLORD <161342908+DANNYSTARLORD@users.noreply.github.com>
2026-02-05 13:39:21 -0500

    Update auth and index CSS, remove header.css

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,5 +1,3 @@
-/* Code written by Pranav Cheruku */
-
 const authModal = document.querySelector('.auth-modal');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
