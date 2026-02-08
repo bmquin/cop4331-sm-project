d57d53da9ab6c963d8e52398730c7d53a57e0acb
DANNYSTARLORD <161342908+DANNYSTARLORD@users.noreply.github.com>
2026-02-07 23:39:14 -0500

    Updated schema.sql

--- schema.sql
+++ schema.sql
@@ -5,6 +5,7 @@ USE contact_manager;
 -- Users
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(25) NOT NULL UNIQUE;
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL, --UNIQUE !!! lol You and user_123 share the same password
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
